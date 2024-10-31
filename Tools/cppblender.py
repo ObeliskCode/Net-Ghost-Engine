249eae1a9a671382cc348bc3a8fa2ec027d2e7f8
ObeliskCode <n6garcia@ucsd.edu>
2024-10-30 20:53:54 -0700

    basis_u build fix

--- Tools/cppblender.py
+++ Tools/cppblender.py
@@ -67,10 +67,9 @@ if not os.path.isdir(BUDIR):
 	cmd = 'git clone --depth 1 https://github.com/BinomialLLC/basis_universal.git'
 	print(cmd)
 	subprocess.check_call(cmd.split())
-	buildir = os.path.join(BUDIR, 'build')
-	os.mkdir(buildir)
-	subprocess.check_call(['cmake', BUDIR], cwd=buildir)
-	subprocess.check_call(['make'], cwd=buildir)
+	# [TODO] fix basis_universal install
+	subprocess.check_call(['cmake', '.'], cwd=BUDIR)
+	subprocess.check_call(['make'], cwd=BUDIR)
 
 BASISU = os.path.join(BUDIR, 'bin/basisu')
 assert os.path.isfile(BASISU)
